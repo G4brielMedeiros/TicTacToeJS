c567f575df0a3dc95b91e99df4917345a318a51c
Gabriel Medeiros <g4brielmedeiros@pm.me>
2021-12-28 00:31:10 -0300

    finished attempt at win logic

--- docs/script.js
+++ docs/script.js
@@ -13,31 +13,76 @@ const gameboard = (() => {
     return board;
   }
 
-  function checkHorizontalWin(mark) {
+  function checkHorizontalWin(playerMark) {
     return board.find((row) => {
       return row.every((col) => {
-        return col == mark;
+        return col == playerMark;
       });
     });
   }
 
-  function checkVerticalWin(mark, col) {
+  function checkVerticalWin(playerMark, col) {
+    
     return board.every((row) => {
-      return row[col] == mark;
+      return row[col] == playerMark;
     });
+    
   }
 
-  return { markBoard, checkHorizontalWin, checkVerticalWin };
+  function checkDiagonalWin(playerMark) {
+    
+    const leftToRight = [];
+    const rightToLeft = [];
+
+
+    board.reduce((offset, row) => {
+      leftToRight.push(row[offset]);
+      return ++offset;
+    }, 0)
+
+    board.reduce((offset, row) => {
+      rightToLeft.push(row[offset]);
+      return --offset;
+    }, 2)
+
+    // console.log(leftToRight);
+    // console.log(rightToLeft);
+
+    const isLeftWinner = leftToRight.every((mark) => {
+      return mark == playerMark;
+    })
+
+    const isRightWinner = rightToLeft.every((mark) => {
+      return mark == playerMark;
+    })
+
+
+
+    return (isLeftWinner || isRightWinner);
+
+
+  }
+
+  function checkWin(playerMark) {
+    const winStatus = [];
+
+  
+
+    return checkHorizontalWin(playerMark) ? true :
+    checkVerticalWin(playerMark, 0) ? true :
+    checkVerticalWin(playerMark, 1) ? true :
+    checkVerticalWin(playerMark, 2) ? true :
+    checkDiagonalWin(playerMark);
+
+  }
+
+  return { markBoard, checkWin };
 })();
 
 
 
-console.table(gameboard.markBoard("X", 0, 0));
-console.table(gameboard.markBoard("O", 1, 1));
+console.table(gameboard.markBoard("X", 0, 2));
+console.table(gameboard.markBoard("X", 1, 1));
 console.table(gameboard.markBoard("X", 2, 0));
-console.table(gameboard.markBoard("O", 2, 1));
-console.table(gameboard.markBoard("X", 1, 0));
 
-console.log(gameboard.checkVerticalWin("X", 0));
-
-console.table(gameboard.moveHistory);
+console.log(gameboard.checkWin('X'));
