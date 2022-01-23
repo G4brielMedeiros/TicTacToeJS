9c83c26ca065ca3274334c335b871ee361d6160a
Gabriel Medeiros <g4brielmedeiros@pm.me>
2022-01-23 03:40:06 -0300

    Commented mystic code from my past self, added stop when player wins, announces winner.

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,12 @@
-const gameboard = (() => {
+const GameLogic = (() => {
+  /*
+    Array that represents the board.
+  */
   let board = [null, null, null, null, null, null, null, null, null];
 
+  /*
+    Combinations of array spots that determine a win.
+  */
   const winCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,43 +18,89 @@ const gameboard = (() => {
     [2, 4, 6],
   ];
 
+  /*
+    Marks the board if {position} isn't already taken, by...
+
+    Sets {spotAvailable} & {winner} variables.
+    If nothing in board position, {spotAvailable} is true.
+    If {spotAvailable} & no {winner}, set board position to player mark.
+    Return state of the move.
+  */
   function markBoard(playerMark, position) {
-    if (board[position] != null) return false;
+    let spotAvailable = false;
+    let winner = getWinner();
 
-    board[position] = playerMark;
+    if (board[position] == null) spotAvailable = true;
 
-    return board;
+    if (spotAvailable && !winner) board[position] = playerMark;
+
+    console.log(board);
+
+    return { spotAvailable, winner };
   }
 
-  function checkPlayerWin(playerMark) {
-    return winCombos.reduce((winner, combo) => {
-      const line = combo.map((spot) => board[spot]);
+  /* checkVictory
+  
+    Checks if a given player won, by...
+    
+    A reduce function with a starting value {isWinner} of 'false' iterates through every {winCombo} in {winCombos}
+    
+      Declarers a constant array mapping the spots in the combo to marks on the board. [0, 1, 2] => [X, X, O]
+      If there's no winning line iterated yet, check if current line is a winning line.
+    Returns {isWinner}
 
-      if (!winner) winner = line.every((spot) => spot == playerMark);
+  */
+  function _checkVictory(playerMark) {
+    return winCombos.reduce((isWinner, winCombo) => {
+      const line = winCombo.map((spot) => board[spot]);
 
-      return winner;
+      if (!isWinner) isWinner = line.every((spot) => spot == playerMark);
+
+      return isWinner;
     }, false);
   }
 
   function getWinner() {
-    return checkPlayerWin("X") ? "X" : checkPlayerWin("O") ? "O" : false;
+    return _checkVictory("X") ? "X" : _checkVictory("O") ? "O" : false;
   }
 
   function resetBoard() {
-    board = board.map(spot => spot = null);
+    board = board.map((spot) => (spot = null));
     return board;
   }
 
   return { markBoard, getWinner, resetBoard };
 })();
 
-console.table(gameboard.markBoard("X", 7));
-console.table(gameboard.markBoard("X", 4));
-console.table(gameboard.markBoard("X", 5));
-console.table(gameboard.markBoard("O", 0));
-console.table(gameboard.markBoard("O", 1));
-gameboard.resetBoard();
-console.table(gameboard.markBoard("O", 2));
+let isXTurn = true;
 
+const boardHTML = document.getElementById("game");
+const statusHTML = document.getElementById("status");
 
-console.log(gameboard.getWinner("X"));
+let cellId = 0;
+
+function createCell() {
+  const gameCellDOM = document.createElement("div");
+  gameCellDOM.classList = "cell";
+  gameCellDOM.id = cellId;
+  cellId++;
+
+  gameCellDOM.addEventListener("click", () => {
+    const playerMove = GameLogic.markBoard(isXTurn ? "X" : "O", gameCellDOM.id);
+    if (playerMove.spotAvailable == true && playerMove.winner == false)
+      gameCellDOM.textContent = isXTurn ? "X" : "O";
+
+    if (GameLogic.getWinner())
+      statusHTML.textContent = `WOOOO! [${GameLogic.getWinner()}] WON THE GAME!`;
+
+    console.log(playerMove);
+
+    isXTurn = !isXTurn;
+  });
+
+  return gameCellDOM;
+}
+
+for (let i = 0; i < 9; i++) {
+  boardHTML.appendChild(createCell());
+}
